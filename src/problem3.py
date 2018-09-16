eaaa66fb3030d339f855faa327fb6c393a5c3eec
scottjm1 <jasminescott2018@gmail.com>
2018-09-16 14:33:39 -0400

    Problem 3

--- src/problem3.py
+++ src/problem3.py
@@ -2,8 +2,8 @@
 PRACTICE Exam 1, problem 3.
 
 Authors: David Mutchler, Vibha Alangar, Valerie Galluzzi, Mark Hays,
-         Amanda Stouder, their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Amanda Stouder, their colleagues and Jasmine Scott.
+"""  # COMPLETED: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -95,6 +95,14 @@ def run_test_problem3a():
     print('Test 4 expected:', expected)
     print('       actual:  ', answer)
 
+    # Test 5 (it is on window 3):
+    point = rg.Point(100, 150)
+    expected = 49
+    answer = problem3a(window3, point, 7)
+    print()
+    print('Test 5 expected:', expected)
+    print('       actual:  ', answer)
+
     window3.close_on_mouse_click()
 
     # ------------------------------------------------------------------
@@ -136,6 +144,28 @@ def problem3a(window, point, n):
         :type point:  rg.Point
         :type n:      int
     """
+    x = point.x
+    y = point.y + 50
+    total = 0
+    point1 = rg.Point(x, y)
+    line = rg.Line(point, point1)
+    line.attach_to(window)
+    for k in range(n):
+        x1 = point.x + (20 * k)
+        y1 = point.y + (10 * k)
+        point3 = rg.Point(x1, y1)
+
+        x2 = x + (20 * k)
+        y2 = y + (10 * k)
+        point4 = rg.Point(x2, y2)
+
+        line2 = rg.Line(point3, point4)
+        line2.thickness = (2 * (k + 1)) - 1
+        line2.attach_to(window)
+        if line2.thickness > 13:
+            line2.thickness = 13
+    window.render()
+
     # ------------------------------------------------------------------
     # TODO: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
